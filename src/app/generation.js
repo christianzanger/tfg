107b16a775d7f10bdc3683d3496358eabada5594
Christian Zanger <christian_zanger@hotmail.com>
2018-07-27 19:43:10 +0200

    Server key handling

--- src/app/generation.js
+++ src/app/generation.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const key = fs.readFileSync('./secret/unsplash', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
-    return data;
+    return data.replace("\n", 'n');
 });
-const URL = `https://api.unsplash.com/search/photos?client_id=${key}&per_page=12&orientation=landscape&query=`;
+const URL = `https://api.unsplash.com/search/photos?client_id=${key}&per_page=12&orientation=landscape&query=`.replace("\n", '');
 // TODO: remove debugging code
 // console.log(`URL: ${URL}`);
 
