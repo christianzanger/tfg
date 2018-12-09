25700c2bb136e67beedbd8f688615b65de088258
Christian Zanger <christian_zanger@hotmail.com>
2018-12-09 08:55:47 +0100

    Add resources table

--- dbsetup.sql
+++ dbsetup.sql
@@ -3,7 +3,6 @@ CREATE TABLE user_history (
     user_id varchar(100),
     avg_load_time DECIMAL(8,2) unsigned,
     loads mediumint,
-    images smallint,
     bytes int,
     bytesSavedByCompression int default 0,
     bytesSavedByCache int default 0,
@@ -19,6 +18,16 @@ CREATE TABLE user_settings (
     cache bit default 0,
     user_id varchar(100),
     tutorial_step tinyint,
+    FOREIGN KEY (history_id) REFERENCES user_history(id) ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE user_resources (
+	id INT NOT NULL auto_increment,
+    history_id int not null,
+	images smallint,
+    javascriptFiles smallint,
+    cssFiles smallint,
     FOREIGN KEY (history_id) REFERENCES user_history(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
